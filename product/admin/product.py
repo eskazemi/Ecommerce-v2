4d97139fbd5ee2b335e249c609034d6f69a2be7a
eskazemi <smk182018@gmail.com>
2023-09-13 09:29:41 +0330

    Feat(): update ProductAdmin
    -create ProductLineInline class
    -add inline field to ProductAdmin class
    -update list display

--- product/admin/product.py
+++ product/admin/product.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
-from product.models import Product
+from product.models import (
+    Product,
+    ProductLine,
+)
+
+
+class ProductLineInline(admin.TabularInline):
+    model = ProductLine
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("name", "brand")
+    inlines = [ProductLineInline]
+    list_display = (
+        "name",
+        "brand",
+        "category",
+        'is_digital',
+        'is_active',
+    )
+    prepopulated_fields = {"slug": ('description',)}
