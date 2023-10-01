f045c49d9ca55e2b92b8bbe55eff47a8271d86d2
Amir Maghami <47292174+amirrr1987@users.noreply.github.com>
2023-10-01 22:41:23 +0330

    feat: add some html tags

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,66 @@
 function App() {
   return (
     <>
-  <div className="">sdf</div>
+      <nav className="bg-blue shadow py-4">
+        <div className="container mx-auto px-4 flex ">
+        <a href=""><img src="" alt="" /></a>
+        <ul className="flex gap-x-4">
+          <li><a href="" className="text-light capitalize">home</a></li>
+          <li><a href="" className="text-light capitalize">About</a></li>
+          <li><a href="" className="text-light capitalize">home</a></li>
+          <li><a href="" className="text-light capitalize">home</a></li>
+        </ul>
+        </div>
+      </nav>
+      <main>
+        <section className="py-4">
+          <div className="container mx-auto px-4">
+            asd
+          </div>
+        </section>
+        <section className="py-4 bg-blue-50">
+          <div className="container mx-auto px-4">
+            asd
+          </div>
+        </section>
+        <section className="py-4">
+          <div className="container mx-auto px-4">
+            asd
+          </div>
+        </section>
+        <section className="py-4 bg-blue-50">
+          <div className="container mx-auto px-4">
+            asd
+          </div>
+        </section>
+        <section className="py-4">
+          <div className="container mx-auto px-4">
+            asd
+          </div>
+        </section>
+        <section className="py-4 bg-blue-50">
+          <div className="container mx-auto px-4">
+            asd
+          </div>
+        </section>
+        <section className="py-4">
+          <div className="container mx-auto px-4">
+            asd
+          </div>
+        </section>
+      </main>
+      <footer>
+        <div className="bg-blue py-4">
+        <div className="container mx-auto px-4">
+            asd
+          </div>
+        </div>
+        <div className="bg-blue-500 py-1">
+        <div className="container mx-auto px-4">
+            asd
+          </div>
+        </div>
+      </footer>
     </>
   )
 }
